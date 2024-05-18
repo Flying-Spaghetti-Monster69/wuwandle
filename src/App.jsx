8571a0d87af7e808955f28484d43a2ff4315569e
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-05-17 19:54:23 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1 className="text-3xl font-bold underline text-red-600">hello</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="sticky bg-stone-900 top-0 h-10"></header>;
+};
+
+export default Header;
