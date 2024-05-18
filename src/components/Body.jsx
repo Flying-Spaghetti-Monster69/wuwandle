169a6fa0660e6477a0be7a4a58694523a647a866
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-05-17 23:24:56 -0500

    navbar styling added

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+const Body = () => {
+  return (
+    <main className="bg-cover image-background h-screen">
+      <Navbar />
+
+      {/* <h1 className="">hello</h1> */}
+    </main>
+  );
+};
+
+export default Body;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
-    <nav className="sticky bg-stone-900 top-0 ">
+    <nav className="sticky bg-gradient-to-b from-black via-[#0c0d0f99] to-transparent top-0 ">
       <div className="mx-auto max-w-7xl px-8 py-2 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-5">
-        <h1 className="bg-gradient-to-tr from-yellow-500 to-zinc-100 bg-clip-text text-transparent text-2xl font-bold font-cinzel w-fit">
+        <h1 className="bg-gradient-to-tr from-yellow-500 to-zinc-100 bg-clip-text text-transparent text-2xl font-bold pl-20 font-cinzel w-fit">
           Wuwandle
         </h1>
       </div>
